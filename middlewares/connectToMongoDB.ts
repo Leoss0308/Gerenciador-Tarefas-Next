fe752a5c62e11c0a598f9e59a44b38b08bd1460f
Leoss0308 <l@l.com>
2022-06-28 10:48:33 -0300

    Conexão remota

--- middlewares/connectToMongoDB.ts
+++ middlewares/connectToMongoDB.ts
@@ -8,7 +8,8 @@ export const connect = (handler: NextApiHandler) =>
             return handler(req, res);
         }
 
-        const DB_CONNECTION_STRING = 'mongodb://localhost:27017/87aoj-gerenciador-tarefas';
+        //const DB_CONNECTION_STRING = 'mongodb://localhost:27017/87aoj-gerenciador-tarefas';
+        const DB_CONNECTION_STRING = 'mongodb+srv://leoss:220277@cluster0.kdha5wb.mongodb.net/?retryWrites=true&w=majority';
         
         mongoose.connection.on('connected', () => console.log('Conectado no banco de dados'));
         mongoose.connection.on('connected', err => console.log('Erro ao conectar no banco de dados'));
